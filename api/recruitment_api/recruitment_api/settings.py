906e9defdbdfdf32e4616d543eb8bcb41892a41c
Wagner Palmeira <wagnerpalmeira.ti@gmail.com>
2022-06-14 05:29:02 -0300

    Change Key

--- api/recruitment_api/recruitment_api/settings.py
+++ api/recruitment_api/recruitment_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9a9ie&tg5!+arog^gd4%dt68^nghd*b&h#ei0(@ad8(k&&kc4^"
+SECRET_KEY = "YOUR_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
