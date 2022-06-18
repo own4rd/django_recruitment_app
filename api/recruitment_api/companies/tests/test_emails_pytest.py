de9b9e9ab8364ba7201849e9227a368f1a5c282d
Wagner Palmeira <wagnerpalmeira.ti@gmail.com>
2022-06-18 18:44:26 -0300

    Refactoring Django unittest to pytest

---
+++ api/recruitment_api/companies/tests/test_emails_pytest.py
@@ -0,0 +1,24 @@
+import json
+from unittest.mock import patch
+from django.core import mail
+
+def test_send_email_should_succeed(mailoutbox, settings) -> None:
+    settings.EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend"
+    assert len(mailoutbox) == 0
+    mail.send_mail(
+        subject="Test Subject",
+        message="Test Message",
+        from_email="teste@teste.com",
+        recipient_list=["teste2@teste.com"],
+        fail_silently=False,
+    )
+    assert len(mailoutbox) == 1
+    assert mailoutbox[0].subject == "Test Subject"
+
+def test_send_email_without_arguments_should_send_empty_email(client) -> None:
+    response = client.post(path='/send-email')
+    response_content = json.loads(response.content)
+    assert response.status_code == 200
+    assert response_content['status'] == 'success'
+    assert response_content['info'] == 'email sent successfully'
+    
