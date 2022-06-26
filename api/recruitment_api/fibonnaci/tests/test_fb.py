849faf4f6eecf4124746853965c52e9edee9bacf
Wagner Palmeira <wagnerpalmeira.ti@gmail.com>
2022-06-26 15:41:59 -0300

    Added Cached Solution

--- api/recruitment_api/fibonnaci/tests/test_fb.py
+++ api/recruitment_api/fibonnaci/tests/test_fb.py
@@ -1,5 +1,6 @@
 from typing import Callable, Dict, List, Tuple
 import pytest
+from fibonnaci.cached import fibonacci_cached, fibonacci_lru_cached
 from fibonnaci.naive import fibonacci_naive
 
 
@@ -43,4 +44,17 @@ def my_parametrized(identifiers: str, values: List[Tuple[int, int]]) -> Decorato
 @my_parametrized(identifiers="n,expected", values=[(0, 0), (1, 1), (2, 1), (20, 6765)])
 def test_naive(n: int, expected: int) -> None:
     res = fibonacci_naive(n)
+    assert res == expected
+
+@my_parametrized(identifiers="n,expected", values=[(0, 0), (1, 1), (2, 1), (20, 6765)])
+def test_cached(n: int, expected: int) -> None:
+    res = fibonacci_cached(n)
+    assert res == expected
+
+
+# Refactor
+@pytest.mark.parametrize("fib_func", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached])
+@pytest.mark.parametrize("n,expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
+def test_fibonacci(fib_func: Callable[[int], int], n: int, expected: int) -> None:
+    res = fib_func(n)
     assert res == expected
