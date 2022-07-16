ace9b613faa1b12bca564fa5b88bdecf94b09622
Wagner Palmeira <wagnerpalmeira.ti@gmail.com>
2022-07-16 19:00:59 -0300

    Mock Tests Examples

--- api/recruitment_api/companies/tests/test_generic_api.py
+++ api/recruitment_api/companies/tests/test_generic_api.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pytest
 
 test_env_companies_url = "http://127.0.0.1:8000/companies/"
 
@@ -25,3 +26,43 @@ def test_create_company_with_layoffs_django_agnostic() -> None:
 def cleanup_company(company_id: str) -> None:
     response = requests.delete(url=f"http://127.0.0.1:8000/companies/{company_id}")
     assert response.status_code == 204
+
+
+# ---------------- API CRIPTO ------------
+
+@pytest.mark.crypto
+def test_dogecoin_api() -> None:
+    response = requests.get(
+        url="https://api.cryptonator.com/api/ticker/btc-usd",
+        headers={'User-Agent': 'Chrome'}
+    )
+
+    assert response.status_code == 200
+    response_content = json.loads(response.content)
+    assert response_content['ticker']['base'] == 'DOGE'
+
+import responses
+@pytest.mark.crypto
+@responses.activate
+def test_mocked_dogecoin_api() -> None:
+    responses.add(method=responses.GET, url="https://api.cryptonator.com/api/ticker/btc-usd", json={
+        "ticker": {
+            "base": "EDEN",
+            "target": "EDEN-USD",
+            "price": "0.04535907",
+            "volume": "0975940509.758770037",
+            "change": "-0.00052372",
+        },
+        "timestamp": 1612515303,
+        "success": True,
+        "error": ""
+    }, status=200)
+    response = requests.get(
+        url="https://api.cryptonator.com/api/ticker/btc-usd",
+        headers={'User-Agent': 'Chrome'}
+    )
+
+    assert response.status_code == 200
+    response_content = json.loads(response.content)
+    assert response_content['ticker']['base'] == 'EDEN'
+    assert response_content['ticker']['target'] == 'EDEN-USD'
